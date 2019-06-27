8c740f359e577240cc409c2111b881b4df1951f7
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-06-27 14:01:00 -0500

    revamped portfolio

---
+++ portfolio/template/default/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from 'Portfolio | Waqar'"; //letter subject
+	$to = 'waqarhabib94@aggienetwork.com'; // Enter Your E-mail
+	$from = 'waqarhabib94@gmail.com'; // Admin e-mail
+	$fromName = 'Waqar Habib | Portfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
